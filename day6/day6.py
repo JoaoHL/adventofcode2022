9823fdb5b20a6036b6c96057881918d142266b43
Joao Henrique <jao.luciano8@gmail.com>
2022-12-28 09:15:12 -0300

    day6: adicionando exemplos e código da primeira parte

---
+++ day6/day6.py
@@ -0,0 +1,9 @@
+
+#parte 1
+with open('input', 'r') as file:
+	transmission = file.read()
+
+	for i in range(len(transmission)-4):
+		if len(set(transmission[i:i+4])) == 4:
+			print(f"Resposta: {i+4}")
+			break
